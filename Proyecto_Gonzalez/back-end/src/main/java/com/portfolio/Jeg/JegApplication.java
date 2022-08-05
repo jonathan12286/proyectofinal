15174a2091e59605c3ce33e3acae4caef032e3a7
jonathan12286 <109044877+jonathan12286@users.noreply.github.com>
2022-08-04 22:45:47 -0300

    Creacion carpetas front-end y Back-end

---
+++ Proyecto_Gonzalez/back-end/src/main/java/com/portfolio/Jeg/JegApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.Jeg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JegApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JegApplication.class, args);
+	}
+
+}
---
+++ Proyecto_Gonzalez/back-end/src/test/java/com/portfolio/Jeg/JegApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.Jeg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JegApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
