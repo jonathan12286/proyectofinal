a9a5b8812f2e75353ae4856075c69b173277771e
jonathan12286 <109044877+jonathan12286@users.noreply.github.com>
2022-08-06 15:22:22 -0300

    uniendo el frontend con el backend

---
+++ Proyecto_Gonzalez/back-end/src/main/java/com/portfolio/Jeg/Repository/IPersonaRepository.java
@@ -0,0 +1,11 @@
+
+package com.portfolio.Jeg.Repository;
+
+import com.portfolio.Jeg.Entity.Persona;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IPersonaRepository extends JpaRepository<Persona,Long> {
+    
+}
