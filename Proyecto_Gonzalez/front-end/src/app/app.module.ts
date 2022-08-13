f9af046fbcaa74c444428a570aca9db69cc4c882
jonathan12286 <109044877+jonathan12286@users.noreply.github.com>
2022-08-13 03:39:34 -0300

    experiencia en front

--- Proyecto_Gonzalez/front-end/src/app/app.module.ts
+++ Proyecto_Gonzalez/front-end/src/app/app.module.ts
@@ -17,6 +17,7 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { interceptorProvider } from './service/interceptor-service';
+import { NewExperienciaComponent } from './components/experiencia/new-experiencia/new-experiencia.component';
 
 @NgModule({
   declarations: [
@@ -31,7 +32,8 @@ import { interceptorProvider } from './service/interceptor-service';
     ProyectoComponent,
     FooterComponent,
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+    NewExperienciaComponent
   ],
   imports: [
     BrowserModule,
